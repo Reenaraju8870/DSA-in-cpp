ff4c35c4e29880bdf8bc818b6eb6e632a2a734ef
Reena Raju <reenaraju4107@gmail.com>
2026-09-14 00:39:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (96.16%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> output;//list
-        output.push_back({});
-        for (int num : nums) {
-            vector<vector<int>> newSubsets; //list
-            for (vector<int> curr : output) {
-                vector<int> temp = curr;
-                temp.push_back(num);
-                newSubsets.push_back(temp);
+        int n = nums.size();
+        int totalSubsets = 1 << n; // 2^n
+        vector<vector<int>> result;
+        result.reserve(totalSubsets);
+
+        // Iterate through all masks from 0 to 2^n - 1
+        for (int mask = 0; mask < totalSubsets; ++mask) {
+            vector<int> subset;
+            for (int i = 0; i < n; ++i) {
+                // Check if the i-th bit in 'mask' is set
+                if (mask & (1 << i)) {
+                    subset.push_back(nums[i]);
+                }
             }
-            for (vector<int> curr : newSubsets) {
-                output.push_back(curr);
-            }
+            result.push_back(move(subset));
         }
-        return output;
+
+        return result;
     }
 };
