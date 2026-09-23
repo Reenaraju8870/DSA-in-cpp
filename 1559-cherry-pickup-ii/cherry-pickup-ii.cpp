c50ecdb4d59000e0ed0c5711e9c782ed8b6e964a
Reena Raju <reenaraju4107@gmail.com>
2026-09-23 07:32:05 +0530

    Time: 36 ms (83.36%) | Memory: 21 MB (29.33%) - LeetSync

---
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int Solver(int i,int j1,int j2,int n, int m,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp){
+        if(j1<0 || j1>=m || j2<0 ||j2>=m) return -1e9;
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+        if(i==n-1){
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
+        }
+       
+
+        int ans = 0;
+        if(j1 == j2) {
+            ans += grid[i][j1];
+        }
+        else {
+            ans += grid[i][j1] + grid[i][j2];
+        }
+
+
+        int futurech=-1e9;
+        for(int a=-1;a<=1;a++){      
+            for(int b=-1;b<=1;b++){
+                int cherries=Solver(i+1,j1+a,j2+b,n,m,grid,dp);
+                futurech=max(futurech,cherries);
+            }
+        }
+        dp[i][j1][j2]=ans+futurech;
+        return dp[i][j1][j2];
+        
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        int ans=Solver(0,0,m-1,n,m,grid,dp);
+        return ans; 
+    }
+};
